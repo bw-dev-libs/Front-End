dbc3721fc01f0a3e8c640741e5c4af8094fdab9a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-24 15:22:27 +0100

    imported NavLink

--- dev-libs/src/components/Navigation.js
+++ dev-libs/src/components/Navigation.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <>
-      <Link to="/signup">Sign-Up</Link>
-      <Link to="/login">Log-in</Link>
-    </>
+    <div>
+      <NavLink to="/signup">Sign-Up</NavLink>
+      <NavLink to="/login">Log-in</NavLink>
+    </div>
   );
 };
 
