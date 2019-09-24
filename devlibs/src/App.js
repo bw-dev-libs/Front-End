92ed3cc786ad6f86e26218819db98d0b1c8f7298
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-24 09:28:02 -0700

    working on cool underline feature for login

--- devlibs/src/App.js
+++ devlibs/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import DevLibPage from '../src/components/DevLibPage';
 import Login from "./components/Login";
-
+ 
 import PrivateRoute from "./components/utils/PrivateRoute";
 
 function App() {
--- devlibs/src/components/Login.js
+++ devlibs/src/components/Login.js
@@ -29,8 +29,28 @@ class Login extends React.Component {
     render() {
   return (
     <form onSubmit={this.handleSubmit}>
-      <h1>Welcome to the Bubble App!</h1>
+      <h1>Dev-Libs!</h1>
       <p>Login in here!</p>
+      <p>By logging in you agree to the ridiculously long terms that you didn't read.</p>
+      <linearGradient
+       
+        id="linearGradient"
+        x1="13"
+        y1="193.49992"
+        x2="307"
+        y2="193.4992"
+        gradientUnits="userSpaceOnUse">
+          <stop
+          style="stop-color:#55af64;"
+          offset="0"
+          id="stop876" />
+          <stop
+          style="stop-color:#f5f5f5;"
+          offset="1"
+          id="stop878" />
+          </linearGradient>
+          <path
+          d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.0016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20.1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
       <input
       type="text"
       name="username"
