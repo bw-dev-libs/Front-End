5baef72ab2fcda423dcce6c4de2d4ab2db034e8b
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-24 09:06:56 -0700

    Errors taken care of

--- devlibs/src/App.js
+++ devlibs/src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import DevLibPage from '../src/components/DevLibPage';
+// import DevLibPage from '../src/components/DevLibPage';
 import Login from "./components/Login";
-import "./styles.scss";
+
 import PrivateRoute from "./components/utils/PrivateRoute";
 
 function App() {
-  const [colorList, setColorList] = useState([]);
+  // const [colorList, setColorList] = useState([]);
   return (
     <Router>
       
       <div className="App">
         <Route exact path="/" component={Login} />
-        <PrivateRoute path="/BubblesPage" component={DevLibPage} />
+        {/* <PrivateRoute path="/BubblesPage" component={DevLibPage} /> */}
       </div>
     </Router>
   );
