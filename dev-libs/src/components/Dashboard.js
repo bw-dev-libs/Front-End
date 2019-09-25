5eeb60b68fc0bc4ac3ff746918818ed428d1da14
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-25 10:06:09 -0700

    I just need data to test this out

--- dev-libs/src/components/Dashboard.js
+++ dev-libs/src/components/Dashboard.js
@@ -1,10 +1,43 @@
 import React from "react";
+import axios from 'axios';
 
-const Dashboard = () => {
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      updateSuccessMessage: '',
+      updateError: '',
+      deleteSuccessMessage: '',
+      deleteError: '',
+    };
+  }
+  update = game => {
+  axios
+  .put('', game)
+  .then(res => {
+    this.setState({
+      updateSuccessMessage: res.data.successMessage,
+      putError: ''
+    });
+  })
+  .catch(err => {
+    this.setState({
+      updateSuccessMessage: '',
+      updateError: err.res.data.Error
+    });
+  }) ; 
+};
+  delete = game => {
+
+  }
+  render(){
   return(
-      <>Dashboard!</>
-      
-  );
+      <>Dashboard!
+      // Games here
+      <button>Update!</button> 
+      <button>Delete</button>
+      </>
+  );}
 };
 
 export default Dashboard
