2fff3c7afc7502164f080cfdbbe8a032672bd507
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-26 09:00:20 -0700

    Routed the edit page

--- dev-libs/src/App.js
+++ dev-libs/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
-
+import WordList from "../src/components/Edit";
 import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-// import Navigation from "./components/Navigation";
+
 import Play from "./components/Play";
 import Dashboard from "./components/Dashboard";
 import LandingPage from "./components/LandingPage";
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Navigation />
+      <Route path="/edit" component={WordList}/>
       <Route exact path="/" component={LandingPage} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
